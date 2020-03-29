1b378ad5a5966852f3b9faec846053f2c81e3d8a
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-03-29 15:00:50 +0200

    add layout

---
+++ src/pages/_layout.svelte
@@ -0,0 +1,19 @@
+<header>
+  <nav>
+    <h1>
+      <a href="/">Show Geek</a>
+    </h1>
+    <ul>
+      <li>
+        <a href="/shows">Recommended shows</a>
+      </li>
+      <li>
+        <a href="/contact">Contact</a>
+      </li>
+    </ul>
+  </nav>
+</header>
+
+<main>
+  <slot />
+</main>
--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,17 +1,4 @@
 
-<header>
-  <nav>
-    <h1>Show Geek</h1>
-    <ul>
-      <li>
-        <a href="/shows">Recommended shows</a>
-      </li>
-      <li>
-        <a href="/contact">Contact</a>
-      </li>
-    </ul>
-  </nav>
-</header>
 
 <main>
   <header>
