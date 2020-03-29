b6a37212ab64474606455b3dd6266ff65d923bc5
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-03-29 14:28:25 +0200

    move header into layout

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,17 +1,3 @@
-<header>
-  <nav>
-    <h1>Movie Geek</h1>
-    <ul>
-      <li>
-        <a href="/movies">Recommended movies</a>
-      </li>
-      <li>
-        <a href="/contact">Contact</a>
-      </li>
-    </ul>
-  </nav>
-</header>
-
 <main>
   <header>
     <h1>Welcome to my movie recommendations.</h1>
