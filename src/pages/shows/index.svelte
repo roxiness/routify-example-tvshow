2fc7b9a01cb1aa9342cd16e284c93d08a74cb95b
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-03-29 15:35:25 +0200

    add params

--- src/pages/shows/index.svelte
+++ src/pages/shows/index.svelte
@@ -1,15 +1,25 @@
+<script>
+  const shows = [
+    [32, "Fargo (2014)"],
+    [179, "The Wire (2002)"],
+    [318, "Community (2009)"],
+    [5, "True Detective (2014)"],
+    [532, "Scrubs (2001)"],
+    [30960, "Cobra Kai (2018)"],
+    [530, "Seinfeld (1989)"],
+    [347, "It's Always Sunny in Philadelphia (2005)"]
+  ];
+</script>
+
 <header>
   <h3>My recommended shows to watch this month are</h3>
   <a href="/shows/suggest">Suggest a show</a>
 </header>
 
 <section>
-  <aside>Fargo (2014)</aside>
-  <aside>The Wire (2002)</aside>
-  <aside>Community (2009)</aside>
-  <aside>True Detective (2014)</aside>
-  <aside>Scrubs (2001)</aside>
-  <aside>Cobra Kai (2018)</aside>
-  <aside>Seinfeld (1989)</aside>
-  <aside>It's Always Sunny in Philadelphia (2005)</aside>
+  {#each shows as [id, name]}
+    <aside>
+      <a href="/shows/{id}">{name}</a>
+    </aside>
+  {/each}
 </section>
