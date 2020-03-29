2e0e905d115cc31e9995b5bea67618d730f0a3b9
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-03-29 17:12:18 +0200

    add navigation

--- src/pages/_layout.svelte
+++ src/pages/_layout.svelte
@@ -1,15 +1,25 @@
+<script>
+  import { url, isActive } from "@sveltech/routify";
+  const links = [["/shows", "Recommended shows"], ["/contact", "Contact"]];
+</script>
+
+<style>
+  .active {
+    border-bottom: 3px solid var(--color-secondary);
+  }
+</style>
+
 <header>
   <nav>
     <h1>
       <a href="/">Show Geek</a>
     </h1>
     <ul>
-      <li>
-        <a href="/shows">Recommended shows</a>
-      </li>
-      <li>
-        <a href="/contact">Contact</a>
-      </li>
+      {#each links as [path, name]}
+        <li>
+          <a href={$url(path)} class:active={$isActive(path)}>{name}</a>
+        </li>
+      {/each}
     </ul>
   </nav>
 </header>
--- src/pages/shows/[id]/_layout.svelte
+++ src/pages/shows/[id]/_layout.svelte
@@ -1,6 +1,12 @@
 <script>
+  import { url, isActive } from "@sveltech/routify";
   import { params } from "@sveltech/routify";
   $: id = $params.id;
+
+  const links = [
+    ["./index", "MAIN"], 
+    ["./details", "DETAILS"]
+  ];
   let show;
 
   async function fetchShow(id) {
@@ -11,12 +17,19 @@
   $: if (id) fetchShow(id);
 </script>
 
+<style>
+  a {
+    padding: 2px 6px 3px;
+  }
+  a.active {
+    background: var(--color-secondary);
+    color: white;
+  }
+</style>
+
 {#if show}
-<div>
-    <a href="/shows/{id}">
-    Main
-    </a>
-    <a href="/shows/{id}/details">Details</a>
+  {#each links as [path, name]}
+    <a href={$url(path)} class:active={$isActive(path)}>{name}</a>
+  {/each}
   <slot scoped={{ show }}>Loading...</slot>
-</div>
 {/if}
