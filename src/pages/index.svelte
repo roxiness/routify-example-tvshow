49d55df704a32717add6a822eebc0ccc3a66285a
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-03-29 14:56:04 +0200

    added inline header to index

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,3 +1,18 @@
+
+<header>
+  <nav>
+    <h1>Show Geek</h1>
+    <ul>
+      <li>
+        <a href="/shows">Recommended shows</a>
+      </li>
+      <li>
+        <a href="/contact">Contact</a>
+      </li>
+    </ul>
+  </nav>
+</header>
+
 <main>
   <header>
     <h1>Welcome to my show recommendations.</h1>
